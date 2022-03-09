c0c5ed6e9e7f67db03b512d9bd3aeb9ff6d9b6a5
Alannah Gregory <gregory.alannah796@gmail.com>
2022-03-08 20:17:59 -0500

    Pushing files to repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BasicInfo from "./components/BasicInfo";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// construtor that takes in props and super method with this.state that equals a empty object
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {},
+    };
+  }
+  //setting up array with dummy information for people
+  render() {
+    const people = [
+      "John",
+      "Miller",
+      "999-999-9999",
+      "01/16/88",
+      "Lisa",
+      "Grey",
+      "888-888-8888",
+      "05/18/92",
+    ];
+    // Hard Challenge
+    //for each person, it will create another li and it will append it to an array and put it in the list
+    const peopleList = people.map((person) => <li key={person}>{person}</li>);
+    return (
+      <div className="App">
+        <BasicInfo />
+        <ul>{peopleList}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/BasicInfo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//function that returns a ul with list items that hold my basic information
+//will import this to App.js
+
+function BasicInfo() {
+  return (
+    <div>
+      <ul>
+        <li>Alannah</li>
+        <li>Gregory</li>
+        <li>777-777-7777</li>
+        <li>00/00/1000</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BasicInfo;
